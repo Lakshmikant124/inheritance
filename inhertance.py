dac12f7a651b1b266b7fca8cb262bbdf758b42c5
Lakshmikant124 <131650511+Lakshmikant124@users.noreply.github.com>
2023-10-06 12:49:31 +0530

    updated code

--- inhertance.py
+++ inhertance.py
@@ -6,5 +6,12 @@ class product:
     def get_product_details(self):
         print(self.name,self.mrp,self.mgdate)
 
+class object(product):
+    def __init__(self,name,mrp,mgdate,expdate,is_sold):
+        super().__init__(name,mrp,mgdate)
+        self.expdate = expdate
+        self.is_sold = is_sold
+        
+
 obj_1 = product("Car",80,"1208")
 obj_1.get_product_details()
