2aaa773c2e1e5aae7a92ec1f09c1b6e002b60620
Lakshmikant124 <lakshmikanthparasa1410@gmail.com>
2023-10-06 12:59:40 +0530

    change saves

--- inhertance.py
+++ inhertance.py
@@ -11,7 +11,10 @@ class object(product):
         super().__init__(name,mrp,mgdate)
         self.expdate = expdate
         self.is_sold = is_sold
+    def get_product_details(self):
+         super().get_product_details()
+         print(self.expdate,self.is_sold)
         
 
-obj_1 = product("Car",80,"1208")
+obj_1 = object("Car",80,"1208","983",True)
 obj_1.get_product_details()
